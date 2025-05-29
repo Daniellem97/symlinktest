0125adb955e9a590256ecb135a3c5465ed0368dd
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-05-29 18:44:20 +0100

    Update main.tf

--- missing-region-stack/main.tf
+++ missing-region-stack/main.tf
@@ -1,2 +1,11 @@
 # Dummy resource so `terraform plan` does something
 resource "null_resource" "example" {}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-buckety89r7gfwuibjlndah8"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
